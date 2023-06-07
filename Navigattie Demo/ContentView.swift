564483be9711af6becc3380929348c07eeb9fe8c
Tomasz Kurylik <fulcrumone@icloud.com>
2023-06-08 00:28:39 +0200

    Initial Commit

---
+++ Navigattie Demo/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift of Navigattie Demo
+//
+//  Created by Tomasz Kurylik
+//    - Twitter: https://twitter.com/tkurylik
+//    - Mail: tomasz.kurylik@mijick.com
+//
+//  Copyright ©2023 Mijick. Licensed under MIT License.
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Navigattie Demo/Navigattie_DemoApp.swift
@@ -0,0 +1,20 @@
+//
+//  Navigattie_DemoApp.swift of Navigattie Demo
+//
+//  Created by Tomasz Kurylik
+//    - Twitter: https://twitter.com/tkurylik
+//    - Mail: tomasz.kurylik@mijick.com
+//
+//  Copyright ©2023 Mijick. Licensed under MIT License.
+
+
+import SwiftUI
+
+@main
+struct Navigattie_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
