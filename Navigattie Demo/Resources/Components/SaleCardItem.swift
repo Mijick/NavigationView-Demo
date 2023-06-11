e048f97ed7430db916cb0f23ec0531f923ed6b3b
Tomasz Kurylik <fulcrumone@icloud.com>
2023-06-11 16:57:54 +0200

    1

---
+++ Navigattie Demo/Resources/Components/SaleCardItem.swift
@@ -0,0 +1,81 @@
+//
+//  SaleCardItem.swift of Navigattie Demo
+//
+//  Created by Tomasz Kurylik
+//    - Twitter: https://twitter.com/tkurylik
+//    - Mail: tomasz.kurylik@mijick.com
+//
+//  Copyright ©2023 Mijick. Licensed under MIT License.
+
+
+import SwiftUI
+
+struct SaleCardItem: View {
+    let data: MockData.SaleCard
+
+
+    var body: some View {
+        Button(action: onItemTap) {
+            VStack(alignment: .leading, spacing: 8) {
+                createImage()
+                createBottomView()
+            }
+            .frame(maxWidth: .infinity)
+        }
+    }
+}
+
+private extension SaleCardItem {
+    func createImage() -> some View {
+        Image(data.image)
+            .resizable()
+            .aspectRatio(contentMode: .fill)
+            .cornerRadius(9)
+    }
+    func createBottomView() -> some View {
+        VStack(alignment: .leading, spacing: 4) {
+            createBottomTopView()
+            createAuthorText()
+        }
+    }
+}
+
+private extension SaleCardItem {
+    func createBottomTopView() -> some View {
+        HStack(spacing: 0) {
+            createTitleText()
+            Spacer()
+            createPriceText()
+        }
+    }
+    func createAuthorText() -> some View {
+        Text(data.author)
+            .font(.regular(12))
+            .foregroundColor(.onBackgroundSecondary)
+            .lineLimit(1)
+            .fixedSize(horizontal: false, vertical: true)
+    }
+}
+
+private extension SaleCardItem {
+    func createTitleText() -> some View {
+        Text(data.title)
+            .font(.semiBold(13))
+            .foregroundColor(.onBackgroundPrimary)
+            .lineLimit(1)
+            .fixedSize(horizontal: false, vertical: true)
+    }
+    func createPriceText() -> some View {
+        Text(data.price, format: .currency(code: "PLN"))
+            .font(.semiBold(13))
+            .foregroundColor(.accentPrimary)
+            .lineLimit(1)
+            .fixedSize(horizontal: false, vertical: true)
+    }
+}
+
+private extension SaleCardItem {
+    func onItemTap() {
+
+    }
+}
